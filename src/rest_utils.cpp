de024d9416dac4a383418cb1d101a3cb05a06391
Ganesh Vernekar <cs15btech11018@iith.ac.in>
2019-03-11 15:42:53 +0530

    Have Json reader and writer local to the thread
    Signed-off-by: Ganesh Vernekar <cs15btech11018@iith.ac.in>

--- src/rest_utils.cpp
+++ src/rest_utils.cpp
@@ -9,11 +9,6 @@
 using namespace nghttp2::asio_http2;
 using namespace nghttp2::asio_http2::client;
 
-namespace {
-Json::FastWriter fastWriter;
-Json::Reader reader;
-}
-
 Json::Value touint64(uint64_t val) { return Json::Value(Json::UInt64(val)); }
 
 Json::Value touint(uint32_t val) { return Json::Value(Json::UInt(val)); }
@@ -24,24 +19,25 @@ Json::Value toint(int val) { return Json::Value(Json::Int(val)); }
 Json::Value tobool(bool val) { return Json::Value(val); }
 
 bool send_and_receive(std::string ip_addr, int port, std::string route, Json::Value &sendPkt, Json::Value &recvPkt) {
-    bool sess_failed = false;
+	bool sess_failed = false;
 	boost::asio::io_service io_service;
 
+	std::string uri = "http://" + ip_addr + ":" + std::to_string(port) + route;
+	Json::FastWriter fastWriter;
 	std::string jsonPkt = fastWriter.write(sendPkt);
 	std::string jsonResponsePkt = "";
+	TRACE(cout << "Sending " << jsonPkt << " to " << uri << endl;)
 
 	session sess(io_service, ip_addr, std::to_string(port));
 
-	sess.on_connect([&sess, &ip_addr, port, &route, &jsonPkt, &jsonResponsePkt, &sess_failed](boost::asio::ip::tcp::resolver::iterator endpoint_it) {
+	sess.on_connect([&sess, &uri, &route, &jsonPkt, &jsonResponsePkt, &sess_failed](boost::asio::ip::tcp::resolver::iterator endpoint_it) {
 		boost::system::error_code ec;
-		std::string uri = "http://" + ip_addr + ":" + std::to_string(port) + route;
-		TRACE(cout << "Sending " << jsonPkt << " to " << uri << endl;)
 		auto req = sess.submit(ec, "POST", uri, jsonPkt);
 
 		req->on_response([&uri, &jsonResponsePkt, &sess, &sess_failed](const response &res) {
 			// TODO handle response errors
 			if(res.status_code() != 200) {
-                sess_failed = true;
+				sess_failed = true;
 				std::cout << "Error Status Code Received from " << uri << " : " << res.status_code() << std::endl;
 			}
 			res.on_data([&jsonResponsePkt, &sess](const uint8_t *data, std::size_t len) {
@@ -60,13 +56,14 @@ bool send_and_receive(std::string ip_addr, int port, std::string route, Json::Va
 	});
 
 	sess.on_error([&sess_failed](const boost::system::error_code &ec) {
-        sess_failed = true;
+		sess_failed = true;
 		std::cerr << "error: " << ec.message() << std::endl;
 	});
 
 	io_service.run();
 
-    if(sess_failed) return false;
+	if(sess_failed) return false;
 
-    return reader.parse(jsonResponsePkt, recvPkt);
+	Json::Reader reader;
+	return reader.parse(jsonResponsePkt, recvPkt);
 }
