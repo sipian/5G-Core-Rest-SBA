6d5adb62133dfd13bdc6db610f07c16871b669d6
sipian <cs15btech11019@iith.ac.in>
2019-03-11 19:13:10 +0530

    add UE registration, session, detach time taken

--- src/ran_simulator.cpp
+++ src/ran_simulator.cpp
@@ -1,12 +1,17 @@
+#include <atomic>
 #include "ran_simulator.h"
 
 time_t g_start_time;
 int g_threads_count;
 uint64_t g_req_dur;
 uint64_t g_run_dur;
-int g_tot_regs;
-uint64_t g_tot_regstime;
-pthread_mutex_t g_mux;
+
+atomic<int> g_tot_regs;
+atomic<uint64_t> g_tot_regstime;
+atomic<uint64_t> ue_registration_tot_regstime;
+atomic<uint64_t> ue_session_tot_regstime;
+atomic<uint64_t> ue_detach_tot_regstime;
+
 vector<thread> g_umon_thread;
 vector<thread> g_dmon_thread;
 vector<thread> g_threads;
@@ -52,6 +57,13 @@ void simulate(int arg) {
 	ran.init(ran_num);
 	ran.conn_mme();
 
+	CLOCK::time_point ue_registration_time_end;
+	CLOCK::time_point ue_session_time_end;
+	CLOCK::time_point ue_detach_time_start;
+	MICROSECONDS ue_registration_diff_us;
+	MICROSECONDS ue_session_diff_us;
+	MICROSECONDS ue_detach_diff_us;
+
 	while (1) {
 		// Run duration check
 		g_utils.time_check(g_start_time, g_req_dur, time_exceeded);
@@ -62,9 +74,15 @@ void simulate(int arg) {
 		// Start time
 		mstart_time = CLOCK::now();	
 		
+		auto t_start = std::chrono::high_resolution_clock::now();
+
 		// Initial attach
 		ran.initial_attach();
-		
+
+		auto t_end = std::chrono::high_resolution_clock::now();
+		double elaspedTimeMs = std::chrono::duration<double, std::milli>(t_end - t_start).count();
+
+
 		// Authentication
 		ok = ran.authenticate();
 		if (!ok) {
@@ -79,12 +97,16 @@ void simulate(int arg) {
 			return;
 		}
 
+		ue_registration_time_end = CLOCK::now();
+
 		// Set eps session
 		ok = ran.set_eps_session(g_traf_mon);
 		if (!ok) {
 			TRACE(cout << "ransimulator_simulate:" << " eps session setup failure" << endl;)
 			return;
 		}
+
+		ue_session_time_end = CLOCK::now();
 
 		/*
 		// To find RTT
@@ -96,6 +118,8 @@ void simulate(int arg) {
 
 		/* Data transfer */
 		ran.transfer_data(g_req_dur);
+
+		ue_detach_time_start = CLOCK::now();
 		// Detach
 		ok = ran.detach();
 		if (!ok) {
@@ -107,13 +131,17 @@ void simulate(int arg) {
 		mstop_time = CLOCK::now();
 		
 		// Response time
+		ue_registration_diff_us = std::chrono::duration_cast<MICROSECONDS>(ue_registration_time_end - mstart_time);
+		ue_session_diff_us = std::chrono::duration_cast<MICROSECONDS>(ue_session_time_end - ue_registration_time_end);
+		ue_detach_diff_us = std::chrono::duration_cast<MICROSECONDS>(mstop_time - ue_detach_time_start);
 		mtime_diff_us = std::chrono::duration_cast<MICROSECONDS>(mstop_time - mstart_time);
 
 		/* Updating performance metrics */
-		g_sync.mlock(g_mux);
 		g_tot_regs++;
-		g_tot_regstime += mtime_diff_us.count();		
-		g_sync.munlock(g_mux);			
+		ue_registration_tot_regstime += ue_registration_diff_us.count();
+		ue_session_tot_regstime += ue_session_diff_us.count();
+		ue_detach_tot_regstime += ue_detach_diff_us.count();
+		g_tot_regstime += mtime_diff_us.count();
 	}
 }
 
@@ -130,7 +158,9 @@ void init(char *argv[]) {
 	g_req_dur = atoi(argv[2]);
 	g_tot_regs = 0;
 	g_tot_regstime = 0;
-	g_sync.mux_init(g_mux);	
+	ue_registration_tot_regstime = 0;
+	ue_session_tot_regstime = 0;
+	ue_detach_tot_regstime = 0;
 	g_umon_thread.resize(NUM_MONITORS);
 	g_dmon_thread.resize(NUM_MONITORS);
 	g_threads.resize(g_threads_count);
@@ -174,13 +204,23 @@ void run() {
 void print_results() {
 	g_run_dur = difftime(time(0), g_start_time);
 	
-	cout << endl << endl;
+	cout << "\n\n";
 	cout << "Requested duration has ended. Finishing the program." << endl;
 	cout << "Total number of registrations is " << g_tot_regs << endl;
 	cout << "Total time for registrations is " << g_tot_regstime * 1e-6 << " seconds" << endl;
+
+	cout << "Total time for UE registrations is " << ue_registration_tot_regstime * 1e-6 << " seconds" << endl;
+	cout << "Average time for UE registrations is " << ((double)ue_registration_tot_regstime / g_tot_regs) * 1e-6 << " seconds" << endl;
+
+	cout << "Total time for UE session is " << ue_session_tot_regstime * 1e-6 << " seconds" << endl;
+	cout << "Average time for UE session is " << ((double)ue_session_tot_regstime / g_tot_regs) * 1e-6 << " seconds" << endl;
+
+	cout << "Total time for UE detach is " << ue_detach_tot_regstime * 1e-6 << " seconds" << endl;
+	cout << "Average time for UE detach is " << ((double)ue_detach_tot_regstime / g_tot_regs) * 1e-6 << " seconds" << endl;
+
 	cout << "Total run duration is " << g_run_dur << " seconds" << endl;
-	cout << "Latency is " << ((double)g_tot_regstime/g_tot_regs) * 1e-6 << " seconds" << endl;
-	cout << "Throughput is " << ((double)g_tot_regs/g_run_dur) << endl;	
+	cout << "Latency is " << ((double)g_tot_regstime / g_tot_regs) * 1e-6 << " seconds" << endl;
+	cout << "Throughput is " << ((double)g_tot_regs / g_run_dur) << endl;
 }
 
 int main(int argc, char *argv[]) {
